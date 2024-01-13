ba3c7cc6d488c8582057d1fc62883f784e8281fd
Poyo663 <jeffersondeazevedomaciel@gmail.com>
2024-01-12 21:14:59 -0300

    added stack data strutures

---
+++ stack.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  void *next;
+  int value;
+} StackNode;
+
+typedef struct {
+  StackNode *top;
+} Stack;
+
+Stack* getStack(){
+  Stack* stack = (Stack*) malloc(sizeof(Stack));
+  stack->top = NULL;
+
+  return stack;
+}
+
+void push(Stack *stack, int value) {
+  StackNode *new = (StackNode *)malloc(sizeof(StackNode));
+  new->value = value;
+
+  if (stack->top == NULL) {
+    stack->top = new;
+    new->next = NULL;
+  } else {
+    new->next = stack->top;
+    stack->top = new;
+  }
+}
+int pop(Stack *stack) {
+  if (stack->top == NULL) {
+    return NULL;
+  }
+  int value = stack->top->value;
+
+  StackNode *current = stack->top;
+  stack->top = stack->top->next;
+  free(current);
+  current = NULL;
+
+  return value;
+}
+int peek(Stack *stack) { return stack->top->value; }
+
+int main(int argc, char *argv[]) {
+  Stack* stack = getStack();
+
+  push(stack, 5);
+  push(stack, 9);
+  push(stack, 6);
+  push(stack, 3);
+
+  printf("%d\n", pop(stack));
+  printf("%d\n", pop(stack));
+  printf("%d\n", pop(stack));
+  printf("%d\n", pop(stack));
+  printf("%d\n", pop(stack));
+
+  return 0;
+}
